483e63e5d94d057bde42a52dbccb50b601ee54a4
Luisa Haase <luisa.haase@plentymarkets.com>
2023-04-11 15:06:27 +0200

    Added gtag snippet from google analytics and additional container link

---
+++ src/Providers/PurchaseEventCodeProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace GoogleAnalytics\Providers;
+
+use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
+use Plenty\Plugin\Templates\Twig;
+
+
+class PurchaseEventCodeProvider
+{
+    public function call( Twig $twig )
+    {
+        /** @var FrontendSessionStorageFactoryContract $sessionStorage */
+        $sessionStorage = pluginApp(FrontendSessionStorageFactoryContract::class);
+
+        $enableTracking = $sessionStorage->getPlugin()->getValue('GA_TRACK_ORDER') === 1;
+
+        $sessionStorage->getPlugin()->unsetKey('GA_TRACK_ORDER');
+
+        return $twig->render(
+            'GoogleAnalytics::GoogleAnalyticsPurchaseEventCode',
+            [
+                'trackOrder' => $enableTracking
+            ]
+        );
+    }
+}
